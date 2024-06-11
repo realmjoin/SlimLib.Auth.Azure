48b99787fe0b311076c55d96c7771b88d8e86552
Jan Berdel <jan.berdel@glueckkanja.com>
2024-06-11 09:34:18 +0200

    Add ISlimClientWithPermissions

---
+++ SlimLib.Auth.Azure/Permissions/ISlimClientWithPermissions.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace SlimLib.Auth.Azure.Permissions;
+
+internal interface ISlimClientWithPermissions
+{
+    public IReadOnlyList<RequiredPermission> Permissions { get; }
+}
