3dc13ee9da7d98e7687c4c1d4d5f16305e57654a
Jan Berdel <jan.berdel@glueckkanja.com>
2024-06-18 07:02:22 +0200

    Add ODataListResponse without Values

--- SlimLib.Auth.Azure/Shared/ODataListResponse.cs
+++ SlimLib.Auth.Azure/Shared/ODataListResponse.cs
@@ -2,14 +2,11 @@
 
 namespace SlimLib.Auth.Azure.Shared;
 
-public class ODataListResponse<T> : ODataResponse
+public class ODataListResponse : ODataResponse
 {
     [JsonPropertyName("@odata.count")]
     public int? ODataCount { get; set; }
 
     [JsonPropertyName("@odata.nextLink")]
     public string? ODataNextLink { get; set; }
-
-    [JsonPropertyName("value")]
-    public T[] ODataValues { get; set; } = null!;
 }
---
+++ SlimLib.Auth.Azure/Shared/ODataListResponse`1.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SlimLib.Auth.Azure.Shared;
+
+public class ODataListResponse<T> : ODataListResponse
+{
+    [JsonPropertyName("value")]
+    public T[] ODataValues { get; set; } = null!;
+}
