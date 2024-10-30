7e8266665957629fdec80cab3d02bcb3f35b246c
Jan Berdel <jan.berdel@glueckkanja.com>
2024-10-30 07:52:33 +0100

    refactor: add multiple missing ConfigureAwait

--- SlimLib.Auth.Azure/TokenProvider/BaseAuthAzureTokenProvider.cs
+++ SlimLib.Auth.Azure/TokenProvider/BaseAuthAzureTokenProvider.cs
@@ -19,13 +19,13 @@ namespace SlimLib.Auth.Azure.TokenProvider
         public async Task<IAuthSuccessResponse> GetTokenAsync(IAzureTenant tenant, IAzureCredentials credentials, string scope)
         {
             using FormUrlEncodedContent content = new(credentials.GetRequestData(scope));
-            using HttpResponseMessage response = await _httpClient.PostAsync(tenant.TokenUrl, content);
-            using Stream stream = await response.Content.ReadAsStreamAsync();
+            using HttpResponseMessage response = await _httpClient.PostAsync(tenant.TokenUrl, content).ConfigureAwait(false);
+            using Stream stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false);
 
             if (response.IsSuccessStatusCode)
             {
                 // {"token_type":"Bearer","scope":"email openid profile https://graph.microsoft.com/User.Read","expires_in":3599,"ext_expires_in":3599,"access_token":"xxx","refresh_token":"xxx","id_token":"xxx"}
-                using JsonDocument doc = await JsonDocument.ParseAsync(stream);
+                using JsonDocument doc = await JsonDocument.ParseAsync(stream).ConfigureAwait(false);
                 return ParseSuccessResponse(doc);
             }
             var error = await JsonSerializer.DeserializeAsync<AuthErrorResponse>(stream).ConfigureAwait(false);
